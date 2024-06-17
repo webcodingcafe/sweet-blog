4f16b2ceb4686d9e503ceea60c7e313cbd54c690
Daniela Kleemann <daniela@webcodingcafe.com>
2024-06-17 21:14:14 +0200

    Implement router in init.php

--- src/init.php
+++ src/init.php
@@ -7,6 +7,35 @@
 
 declare(strict_types=1);
 
+use SweetBlog\Core\Router;
+
 require __DIR__ . '/Autoloader.php';
 
 (new \SweetBlog\Autoloader('SweetBlog', __DIR__))->register();
+
+$router = new Router(
+    new \SweetBlog\Core\Request(),
+    new \SweetBlog\Core\Routes(),
+);
+
+try {
+    $controllerClass = $router->resolve();
+
+    if (!class_exists($controllerClass)) {
+        throw new Exception("Class {$controllerClass} does not exist.");
+    }
+
+    if (!method_exists($controllerClass, '__invoke')) {
+        throw new Exception("Class {$controllerClass} does not have a __invoke method.");
+    }
+
+    (new $controllerClass())();
+} catch (\SweetBlog\Exception\HttpNotFoundException $e) {
+    http_response_code(404);
+    error_log($e->getMessage());
+    echo '404 Not Found';
+} catch (Exception $e) {
+    http_response_code(500);
+    error_log($e->getMessage());
+    echo 'Something went wrong.';
+}
