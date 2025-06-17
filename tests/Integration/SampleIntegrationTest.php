31b6d53649dede3ce198048dc876cd45e5485e18
Daniela Kleemann <ela@webcodingcafe.com>
2025-06-17 21:47:55 +0200

    chore: add a sample integration test

---
+++ tests/Integration/SampleIntegrationTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Integration;
+
+use PHPUnit\Framework\Attributes\CoversNothing;
+use Tests\BaseIntegrationTestCase;
+
+#[CoversNothing]
+class SampleIntegrationTest extends BaseIntegrationTestCase
+{
+    public function testHttpRequest(): void
+    {
+        $response = self::$client->get('/', ['http_errors' => false]);
+
+        $this->assertNotEmpty($response->getHeaders());
+    }
+}
